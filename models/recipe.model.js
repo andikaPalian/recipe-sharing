8be72e72193383c6f03f614d2cd3d8f2e4ff30e7
Andika Palian <andika12des@gmail.com>
2025-01-21 20:48:50 +0800

    Add recipe model with schema definition for recipes

---
+++ models/recipe.model.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema({
+    title: { 
+        type: String,
+        required: true 
+    },
+    description: {
+        type: String, 
+        required: true 
+    },
+    ingredients: {
+        type: [String], 
+        required: true 
+    },
+    steps: { 
+        type: [String], 
+        required: true
+    },
+    prepTime: { 
+        type: Number 
+    },
+    cookTime: { 
+        type: Number 
+    },
+    servings: { 
+        type: Number 
+    },
+    category: { 
+        type: String 
+    },
+    image: { 
+        type: String 
+    },
+    tags: { 
+        type: [String] 
+    },
+    author: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Chef", 
+        required: true 
+    },
+    isPublic: { 
+        type: Boolean, 
+        default: true 
+    },
+    likes: { 
+        type: Number, 
+        default: 0 
+    },
+    comments: [{
+        user: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "Chef" },
+        comment: { 
+            type: String 
+        },
+        createdAt: { 
+            type: Date, 
+            default: Date.now 
+        },
+    }],
+}, {
+    timestamps: true,
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+export default Recipe;
